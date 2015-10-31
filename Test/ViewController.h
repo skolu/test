924ff2ddf2cb242f6f2cfc549e3aa369450b120d
Sergey Kolupaev <skolupaev@gmail.com>
2015-10-30 20:53:38 -0700

    Initial Commit

---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Sergey Kolupaev on 10/30/15.
+//  Copyright © 2015 Sergey Kolupaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by Sergey Kolupaev on 10/30/15.
+//  Copyright © 2015 Sergey Kolupaev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
